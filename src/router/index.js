8d9fcadbc95db152062f504c1581b9a498d799bf
郝仕兢 <haoshijing@corp.netease.com>
2018-04-25 09:30:08 +0800

    菜单增加

--- src/router/index.js
+++ src/router/index.js
@@ -46,28 +46,128 @@ export default new Router({
 
 export const asyncRouterMap = [
   {
+    path: '/data',
+    component: Layout,
+    name: 'data',
+    meta: {
+      title: '数据分析',
+      icon: 'component'
+    },
+    children: [
+      { path: 'orderlist', component: _import('extend/empty'), name: 'orderlist', meta: { title: '订单分析' }},
+      { path: 'userlist', component: _import('extend/empty'), name: 'userlist', meta: { title: '用户分析' }},
+      { path: 'devicedatalist', component: _import('extend/empty'), name: 'userlist', meta: { title: '数据分析' }},
+      { path: 'system', component: _import('extend/empty'), name: 'userlist', meta: { title: '系统分析' }}
+    ]
+  },
+  {
     path: '/device',
     component: Layout,
-    name: '设备管理',
+    name: 'device',
     meta: {
       title: '设备管理',
       icon: 'component'
     },
     children: [
       { path: 'deviceList', component: _import('extend/device/index'), name: '设备列表', meta: { title: '设备列表' }},
-      { path: 'deviceType', component: _import('extend/device/index'), name: '设备类型管理', meta: { title: '设备类型管理' }}
+      { path: 'deviceMap', component: _import('extend/empty'), name: '设备地图展示', meta: { title: '设备地图展示' }}
     ]
   },
   {
-    path: '/project',
+    path: '/device',
     component: Layout,
-    name: '工程管理',
+    name: 'device',
     meta: {
-      title: '工程管理',
+      title: '设备管理',
       icon: 'component'
     },
     children: [
-      { path: 'projectList', component: _import('extend/content/index'), name: '工程列表', meta: { title: '工程列表' }}
+      { path: 'deviceList', component: _import('extend/device/index'), name: '设备列表', meta: { title: '设备列表' }},
+      { path: 'deviceMap', component: _import('extend/empty'), name: '设备地图展示', meta: { title: '设备地图展示' }}
+    ]
+  },
+
+  {
+    path: '/sale',
+    component: Layout,
+    name: 'sale',
+    meta: {
+      title: '售后管理',
+      icon: 'component'
+    },
+    children: [
+      { path: 'alarm', component: _import('extend/empty'), name: '告警设备列表', meta: { title: '告警设备列表' }},
+      { path: 'alarmLevel', component: _import('extend/empty'), name: '告警级别管理', meta: { title: '告警级别管理' }},
+    ]
+  },
+  {
+    path: '/order',
+    component: Layout,
+    name: '订单中心',
+    meta: {
+      title: '订单中心',
+      icon: 'component'
+    },
+    children: [
+      { path: 'saleOrder12', component: _import('extend/empty'), name: '租赁订单列表', meta: { title: '租赁订单列表' }},
+      { path: 'projectList11', component: _import('extend/empty'), name: '租赁充值列表', meta: { title: '租赁充值列表' }},
+      { path: 'projectList111', component: _import('extend/empty'), name: '分销订单', meta: { title: '分销订单' }},
+      { path: 'projectList1111', component: _import('extend/empty'), name: '售后订单', meta: { title: '售后订单' }}
+    ]
+  },
+  {
+    path: '/order',
+    component: Layout,
+    name: '订单中心',
+    meta: {
+      title: '订单中心',
+      icon: 'component'
+    },
+    children: [
+      { path: 'saleOrder', component: _import('extend/empty'), name: '租赁订单列表', meta: { title: '租赁订单列表' }},
+      { path: 'projectList5', component: _import('extend/empty'), name: '租赁充值列表', meta: { title: '租赁充值列表' }},
+      { path: 'projectList6', component: _import('extend/empty'), name: '分销订单', meta: { title: '分销订单' }},
+      { path: 'projectList7', component: _import('extend/empty'), name: '售后订单', meta: { title: '售后订单' }}
+    ]
+  },
+  {
+    path: '/profit',
+    component: Layout,
+    name: '分润管理',
+    meta: {
+      title: '分润管理',
+      icon: 'component'
+    },
+    children: [
+      { path: 'saleOrder2', component: _import('extend/empty'), name: '租赁分润账单', meta: { title: '租赁分润账单' }},
+      { path: 'projectList1', component: _import('extend/empty'), name: '售后分润订单', meta: { title: '售后分润订单' }},
+      { path: 'projectList2', component: _import('extend/empty'), name: '分销分润订单', meta: { title: '分销分润订单' }},
+      { path: 'projectList3', component: _import('extend/empty'), name: '售后订单', meta: { title: '售后分润订单' }},
+      { path: 'projectList4', component: _import('extend/empty'), name: '售后订单', meta: { title: '售后分润账单' }},
+    ]
+  },
+  {
+    path: '/chuzu',
+    component: Layout,
+    name: '租赁管理',
+    meta: {
+      title: '租赁管理',
+      icon: 'component'
+    },
+    children: [
+
+    ]
+  },
+  {
+    path: '/system',
+    component: Layout,
+    name: '系统管理',
+    meta: {
+      title: '系统管理',
+      icon: 'component'
+    },
+    children: [
+
     ]
   },
   { path: '*', redirect: '/404', hidden: true }
